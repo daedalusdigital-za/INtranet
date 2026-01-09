37ba9ed5a4cae75b2d5d11965c624dbc87409e80
Daedalus <107707715+daedalusdigital-za@users.noreply.github.com>
2026-01-09 01:40:57 -0800

    Update build artifacts

--- Backend/obj/Debug/net9.0/ProjectTracker.API.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/ProjectTracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8ca703611f10c80cea12db1d1fff09ecd53c641")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eee1eef2daf2161e5928e90e9db1b8c97231f394")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
